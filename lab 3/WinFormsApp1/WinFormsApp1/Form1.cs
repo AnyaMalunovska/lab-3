823ca9881613d16dd05ea8dac7735b095c2daa88
AnyaMalunovska <126598234+AnyaMalunovska@users.noreply.github.com>
2023-02-28 18:26:54 +0200

    Add files via upload

---
+++ lab 3/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,24 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Singleton.Instance.Log("Pressed button1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Singleton.Instance.Log("Pressed button2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Singleton.Instance.Log("Pressed button3");
+        }
+    }
+}
---
+++ lab 3/WinFormsApp1/WinFormsApp1/Singleton.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+        private StreamWriter writer;
+
+        Singleton()
+        {
+            string fileName = "log.txt";
+            string path = @"C:\MyTextFiles";
+            string fullPath = Path.Combine(path, fileName);
+
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                if (File.Exists(fullPath))
+                {
+                    File.Delete(fullPath);
+                }
+
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string filePath = @"C:\log.txt";
+
+            writer = new StreamWriter(filePath, true);
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            writer.WriteLine(DateTime.Now + ": " + message);
+            writer.Flush();
+        }
+    }
+}
